e8bccd33467da95a320baa3dc564091e9c880b15
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 03:34:27 +0330

    Update DinoException.php

--- src/Exceptions/DinoException.php
+++ src/Exceptions/DinoException.php
@@ -1,1 +1,21 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Dino\Exceptions;
+
+class DinoException extends \Exception
+{
+    protected int $errorCode;
+
+    public function __construct(string $message = "Dino Library Exception", int $code = 1000)
+    {
+        parent::__construct($message, $code);
+        $this->errorCode = $code;
+    }
+
+    public function getErrorCode(): int
+    {
+        return $this->errorCode;
+    }
+}
