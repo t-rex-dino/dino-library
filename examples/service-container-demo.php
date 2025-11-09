60e93b9b3326ff9248f53b7792143e6793e535cb
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 04:27:00 +0330

    Update service-container-demo.php

--- examples/service-container-demo.php
+++ examples/service-container-demo.php
@@ -1,1 +1,27 @@
+<?php
 
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dino\Core\ServiceContainer;
+use Dino\Contracts\FactoryInterface;
+
+class LoggerFactory implements FactoryInterface
+{
+    public function create(mixed ...$parameters): object
+    {
+        return new class {
+            public function log(string $message): void
+            {
+                echo "[LOG] " . $message . PHP_EOL;
+            }
+        };
+    }
+}
+
+$container = new ServiceContainer();
+$container->addFactory('logger', new LoggerFactory());
+
+$logger = $container->get('logger');
+$logger->log("ServiceContainer is working!");
