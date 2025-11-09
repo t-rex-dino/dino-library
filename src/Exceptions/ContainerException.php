26c93d8b93f1dfe2ed63543aff9f160b8efe1291
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 03:36:40 +0330

    Update ContainerException.php

--- src/Exceptions/ContainerException.php
+++ src/Exceptions/ContainerException.php
@@ -1,1 +1,13 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Dino\Exceptions;
+
+class ContainerException extends DinoException
+{
+    public function __construct(string $message = "Container error", int $code = 3001)
+    {
+        parent::__construct($message, $code);
+    }
+}
