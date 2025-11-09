775eab5b5201e7e66a1dff8006662cc76b114a08
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 03:49:03 +0330

    Update ConfigHandler.php

--- src/Core/ConfigHandler.php
+++ src/Core/ConfigHandler.php
@@ -1,1 +1,31 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Dino\Core;
+
+use Dino\Exceptions\ConfigurationException;
+
+class ConfigHandler
+{
+    protected array $config = [];
+
+    public function set(string $key, mixed $value): void
+    {
+        $this->config[$key] = $value;
+    }
+
+    public function get(string $key): mixed
+    {
+        if (!array_key_exists($key, $this->config)) {
+            throw new ConfigurationException("Configuration key '{$key}' not found.");
+        }
+
+        return $this->config[$key];
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->config);
+    }
+}
