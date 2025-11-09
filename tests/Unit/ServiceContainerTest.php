9ad9be78c8e8470c29da63bebb7175b593780977
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 04:10:20 +0330

    Update ServiceContainerTest.php

--- tests/Unit/ServiceContainerTest.php
+++ tests/Unit/ServiceContainerTest.php
@@ -1,1 +1,51 @@
+<?php
 
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Dino\Core\ServiceContainer;
+use Dino\Contracts\FactoryInterface;
+use Dino\Exceptions\ContainerException;
+
+final class ServiceContainerTest extends TestCase
+{
+    public function testAddFactoryAndCreateService(): void
+    {
+        $container = new ServiceContainer();
+
+        $factory = new class implements FactoryInterface {
+            public function create(mixed ...$parameters): object
+            {
+                return new stdClass();
+            }
+        };
+
+        $container->addFactory('test', $factory);
+        $this->assertInstanceOf(stdClass::class, $container->get('test'));
+    }
+
+    public function testHasFactory(): void
+    {
+        $container = new ServiceContainer();
+
+        $factory = new class implements FactoryInterface {
+            public function create(mixed ...$parameters): object
+            {
+                return new stdClass();
+            }
+        };
+
+        $container->addFactory('test', $factory);
+
+        $this->assertTrue($container->has('test'));
+        $this->assertFalse($container->has('missing'));
+    }
+
+    public function testGetMissingFactoryThrowsException(): void
+    {
+        $this->expectException(ContainerException::class);
+
+        $container = new ServiceContainer();
+        $container->get('unknown');
+    }
+}
