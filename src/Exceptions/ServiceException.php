3061a97bdf9e9dbcd95bdc5211fd24ad26027a29
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 03:35:54 +0330

    Update ServiceException.php

--- src/Exceptions/ServiceException.php
+++ src/Exceptions/ServiceException.php
@@ -1,1 +1,13 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Dino\Exceptions;
+
+class ServiceException extends DinoException
+{
+    public function __construct(string $message = "Service error", int $code = 2001)
+    {
+        parent::__construct($message, $code);
+    }
+}
