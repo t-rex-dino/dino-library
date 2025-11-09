8d72d76eac07655bb6ae9e1d76374fe4275a0f43
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 03:50:04 +0330

    Update ServiceContainer.php

--- src/Core/ServiceContainer.php
+++ src/Core/ServiceContainer.php
@@ -1,1 +1,32 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Dino\Core;
+
+use Dino\Contracts\FactoryInterface;
+use Dino\Exceptions\ContainerException;
+
+class ServiceContainer
+{
+    protected array $factories = [];
+
+    public function addFactory(string $name, FactoryInterface $factory): void
+    {
+        $this->factories[$name] = $factory;
+    }
+
+    public function get(string $name, mixed ...$parameters): object
+    {
+        if (!isset($this->factories[$name])) {
+            throw new ContainerException("Factory '{$name}' not found.");
+        }
+
+        return $this->factories[$name]->create(...$parameters);
+    }
+
+    public function has(string $name): bool
+    {
+        return isset($this->factories[$name]);
+    }
+}
