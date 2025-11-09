109d5e73d9a52d01091746d303cd2485b8a4310b
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 04:25:55 +0330

    Update config-handler-demo.php

--- examples/config-handler-demo.php
+++ examples/config-handler-demo.php
@@ -1,1 +1,14 @@
+<?php
 
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dino\Core\ConfigHandler;
+
+$config = new ConfigHandler();
+$config->set('database.host', 'localhost');
+$config->set('database.port', 3306);
+
+echo "Host: " . $config->get('database.host') . PHP_EOL;
+echo "Port: " . $config->get('database.port') . PHP_EOL;
