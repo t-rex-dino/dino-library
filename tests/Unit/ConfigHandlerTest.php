80223f9e04136263757b42519778f5ebbd4d9b0f
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 04:09:30 +0330

    Update ConfigHandlerTest.php

--- tests/Unit/ConfigHandlerTest.php
+++ tests/Unit/ConfigHandlerTest.php
@@ -1,1 +1,35 @@
+<?php
 
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Dino\Core\ConfigHandler;
+use Dino\Exceptions\ConfigurationException;
+
+final class ConfigHandlerTest extends TestCase
+{
+    public function testSetAndGetConfig(): void
+    {
+        $config = new ConfigHandler();
+        $config->set('key', 'value');
+
+        $this->assertSame('value', $config->get('key'));
+    }
+
+    public function testHasConfig(): void
+    {
+        $config = new ConfigHandler();
+        $config->set('key', 'value');
+
+        $this->assertTrue($config->has('key'));
+        $this->assertFalse($config->has('missing'));
+    }
+
+    public function testGetMissingConfigThrowsException(): void
+    {
+        $this->expectException(ConfigurationException::class);
+
+        $config = new ConfigHandler();
+        $config->get('unknown');
+    }
+}
