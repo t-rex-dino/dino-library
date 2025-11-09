75e0b0b062cc357734a4d2e1ca23084563cb6edf
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 03:44:29 +0330

    Update LibraryManager.php

--- src/Core/LibraryManager.php
+++ src/Core/LibraryManager.php
@@ -1,1 +1,29 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Dino\Core;
+
+class LibraryManager
+{
+    protected array $services = [];
+
+    public function register(string $name, object $service): void
+    {
+        $this->services[$name] = $service;
+    }
+
+    public function get(string $name): object
+    {
+        if (!isset($this->services[$name])) {
+            throw new \Dino\Exceptions\ServiceException("Service '{$name}' not found.");
+        }
+
+        return $this->services[$name];
+    }
+
+    public function has(string $name): bool
+    {
+        return isset($this->services[$name]);
+    }
+}
