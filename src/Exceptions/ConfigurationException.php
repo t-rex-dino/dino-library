cf87f05ab2683831235ad6938a00ad8561c6f849
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 03:35:14 +0330

    Update ConfigurationException.php

--- src/Exceptions/ConfigurationException.php
+++ src/Exceptions/ConfigurationException.php
@@ -1,1 +1,13 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Dino\Exceptions;
+
+class ConfigurationException extends DinoException
+{
+    public function __construct(string $message = "Configuration error", int $code = 1001)
+    {
+        parent::__construct($message, $code);
+    }
+}
