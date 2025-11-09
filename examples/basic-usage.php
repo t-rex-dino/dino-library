eda5d1b74d89912ec516f532b22086ea69d1c0f1
t-rex-dino <133510439+t-rex-dino@users.noreply.github.com>
2025-11-09 04:27:44 +0330

    Update basic-usage.php

--- examples/basic-usage.php
+++ examples/basic-usage.php
@@ -1,1 +1,18 @@
+<?php
 
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dino\Core\LibraryManager;
+
+$manager = new LibraryManager();
+$manager->register('greeting', new class {
+    public function sayHello(): void
+    {
+        echo "Hello from Dino Library!" . PHP_EOL;
+    }
+});
+
+$service = $manager->get('greeting');
+$service->sayHello();
